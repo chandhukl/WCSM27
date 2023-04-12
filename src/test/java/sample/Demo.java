34ad536f47e0c741b52de841543a93ddbb4fe851
poorvik <chasl2199@gmail.com>
2023-04-12 12:02:52 +0530

    demo is committed

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -1,11 +1,12 @@
 package sample;
 
+import org.testng.annotations.Test;
+
 public class Demo {
 
-	public static void main(String[] args) {
-
-		System.out.println("demo in git");
-
+	@Test
+	
+	public void demo() {
+		System.out.println("github_demo");
 	}
-
 }
